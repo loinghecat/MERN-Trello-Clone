b50969e6724ca3549d4b11670eb8dc226d000a02
loinghelinux <loinghecat@gmail.com>
2024-10-09 17:47:08 -0500

    Start fetching API from backend

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,18 +1,25 @@
 
 
 import Container from '@mui/material/Container'
-
+import { useEffect, useState } from 'react'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import {mockData} from '~/apis/mock-data'
 
+import {fetchBoardDetailsAPI} from '~/apis/index'
 function Board() {
+  const [board, setBoard] = useState(null)
+  useEffect(() => {
+    const boardId='6706ee0e63b3f603a4c57e6f'
+    fetchBoardDetailsAPI(boardId).then((data) => {
+      setBoard(data)
+    })
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar/>
-      <BoardBar board={mockData?.board}/>
-      <BoardContent board={mockData?.board}/>
+      <BoardBar board={board}/>
+      <BoardContent board={board}/>
     </Container>
   )
 }
