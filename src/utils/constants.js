225af34292ad4e6f8711089103f8515d8b23ee15
loinghelinux <loinghecat@gmail.com>
2024-10-16 19:24:42 -0500

    Updated related files for deploying Frontend to Vercel and Backend to Render

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,9 @@
-//export const API_ROOT = 'http://localhost:8000'
-export const API_ROOT = 'https://mern-trello-api.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8000'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://mern-trello-api.onrender.com'
+}
+
+export const API_ROOT = apiRoot
